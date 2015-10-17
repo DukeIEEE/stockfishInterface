67707b1705685c56d467f0404418d79af79d06a4
Sivaneshwaran Loganathan <sivaneshwaran.loganathan@duke.edu>
2015-10-17 16:08:37 -0400

    bug found: need to keep POpen open

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 from subprocess import Popen, PIPE
+import time
 exe_str = "stockfish-6-win\Windows\stockfish-6-64.exe"
 
+blackMoves = ['h7h6', 'g7g6', 'e7e6'];
+moves=[];
+
+def getMoves():
+    tempStr = '';
+    for x in moves:
+        tempStr += x + " ";
+    return tempStr;
 
 parent = Popen(exe_str,  stderr=PIPE, stdin=PIPE, stdout=PIPE); 
-output2 = parent.communicate('go')
-print output2[0]
-parent = Popen(exe_str,  stderr=PIPE, stdin=PIPE, stdout=PIPE);
-output= parent.communicate('uci')
-print output[0]
+parent.stdout.flush()
 
+for x in blackMoves:
+    parent.stdin.write('go')
+    output = parent.stdout.readline()
+    print output
+    moves.append(output.split()[-1])
+    moves.append(x) 
+    sendString = "position startmov moves %s" % getMoves();
+    print sendString
+    parent.stdin.write(sendString);
+
+
+
+print moves
