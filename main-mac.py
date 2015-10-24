caf157f526bff426226bc603b2678ba83a7cbe8f
Phil <philphoo95@gmail.com>
2015-10-24 19:15:59 -0400

    tested with sample inputs

--- main-mac.py
+++ main-mac.py
@@ -1,27 +1,35 @@
 import pexpect
+
+def getbest(stockfishoutput):
+    stockfisharray = stockfishoutput.split(" ")
+    bestmove = stockfisharray[len(stockfisharray)-2]
+    return bestmove
+
 addr = 'stockfish-6-mac/Mac/stockfish-6-64'
+command = "position startpos moves"
+moveList = ["g7g6", "a7a5", "h7h5", "g8f6"]
 
 child = pexpect.spawn(addr)
-
 child.sendline('uci')
-child.sendline('go')
-child.expect_exact("ponder ")
 
-print child.before
-print ("\n\n")
+for i in range(0, len(moveList)):
+    child.sendline('go')
+    child.expect_exact("ponder ")
+    bestmove = getbest(child.before)
+    print("Computer's move: " + bestmove + "\n")
+    #add computer move
+    command += (" " + bestmove)
+    print "Command: "+ command + "\n"
 
-child.sendline("position startpos moves g1f3")
-child.sendline("go")
-child.expect_exact("ponder ")
+    #add person move
+    command += (" " + moveList[i])
+    print("Person's move: " + moveList[i] + "\n")
+    print "Command: " + command + "\n"
+    child.sendline(command)
 
-print child.before
-print ("\n\n")
 
-child.sendline("position startpos moves g1f3 d7d5")
-child.sendline("go")
-child.expect_exact("ponder ")
 
-print child.before
+
 
 
 
